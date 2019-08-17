18b64b1adb476be887df999b964ca8ea9186063c
alessanderleite <alessander.logistica@gmail.com>
2019-08-17 16:00:57 -0300

    create the controller class named TodoController

---
+++ src/main/java/br/com/alessanderleite/controller/TodoController.java
@@ -0,0 +1,22 @@
+package br.com.alessanderleite.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alessanderleite.TodoClient;
+import br.com.alessanderleite.model.TodoModel;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+	@Autowired
+	TodoClient todoClient;
+	
+	public List<TodoModel> getTodos() {
+		return todoClient.getTodos();
+	}
+}
