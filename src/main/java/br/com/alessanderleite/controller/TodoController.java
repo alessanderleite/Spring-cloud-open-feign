b3952e7c68d8872cc9be0a99d0ef1004c6fd2b45
alessanderleite <alessander.logistica@gmail.com>
2019-08-17 16:14:24 -0300

    add @GetMapping() annotation

--- src/main/java/br/com/alessanderleite/controller/TodoController.java
+++ src/main/java/br/com/alessanderleite/controller/TodoController.java
@@ -3,6 +3,7 @@ package br.com.alessanderleite.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ public class TodoController {
 	@Autowired
 	TodoClient todoClient;
 	
+	@GetMapping()
 	public List<TodoModel> getTodos() {
 		return todoClient.getTodos();
 	}
